22ae75be68e634471f397cd9f0a59e4d1134e313
cw01483-ly <cw01483@gmail.com>
2025-09-29 21:00:25 +0900

    html추가(nav,post_list)

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")// 브라우저에서 http://localhost:8080/ 요청이 오면
+    @GetMapping("/")// 브라우저에서 http://localhost:8008/ 요청이 오면
     public String home(){
         return "index";
         // resources/templates/index.html 파일을 찾아서 보여줌
