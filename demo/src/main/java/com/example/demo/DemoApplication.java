076bb07b3e0896c98cfb11d4ce0f136038850046
cw01483-ly <cw01483@gmail.com>
2025-10-01 21:50:41 +0900

    향후계획

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+    /*앞으로 추가할 목록
+     1. 게시글 PK값이용이 아닌 DisplatNumber부여 후 사용
+     2. 회원탈퇴기능, 관리자or사용자 설정 및 권한부여
+     3. 게시글에 좋아요 누르는 기능?
+     */
 }
